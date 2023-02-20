2fcf6d88911963f52b78c5224a09ba7f9dee5515
mohamed <m635379@gmail.com>
2023-02-20 17:11:37 +0200

    add signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Utilities/Footer";
 import NavBar from "./components/Utilities/NavBar";
 import LoginPage from "./pages/Auth/LoginPage";
+import SignupPage from "./pages/Auth/SignupPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Auth/SignupPage.jsx
+++ src/pages/Auth/SignupPage.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
-    <div className="login-page" style={{ height: "calc(100vh - 144px)" }}>
+    <div className="signup-page" style={{ height: "calc(100vh - 144px)" }}>
       <Container>
         <Form
           style={{ width: "50%", margin: "20px auto", textAlign: "center" }}
         >
-          <h1>Log In</h1>
+          <h1>Sign Up</h1>
+          <Form.Group className="mb-3" controlId="formBasictext">
+            <Form.Control type="text" placeholder="Enter username" />
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
@@ -17,12 +20,12 @@ const LoginPage = () => {
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
           <Button className="w-100" variant="dark" type="submit">
-            Login
+            Signup
           </Button>
         </Form>
         <div className="info w-50 m-auto text-center">
           <p>
-            Have'nt an account? <Link to="/signup">Sign Up</Link>
+            Already have an account? <Link to="/login">Login</Link>
           </p>
         </div>
       </Container>
@@ -30,4 +33,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
