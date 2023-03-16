6bc4bd9a96d72fd447d56506dd199c3fea0cd67f
mohamed <m635379@gmail.com>
2023-03-16 16:03:47 +0200

    add productsReducer with type setProduct

---
+++ src/redux/reducers/productsReducer.js
@@ -0,0 +1,21 @@
+import { SET_PRODUCT } from "../types";
+
+const intialState = {
+  products: [],
+  loading: true,
+  response: {},
+};
+
+const productsReducer = (state = intialState, action) => {
+  switch (action.type) {
+    case SET_PRODUCT:
+      return {
+        response: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import brandsReducer from "./brandsReducer";
 import categoriesReducer from "./categoriesReducer";
+import productsReducer from "./productsReducer";
 import subCategoriesReducer from "./subCategoriesReducer";
 
 const rootReducer = combineReducers({
   categories: categoriesReducer,
   brands: brandsReducer,
   subCategories: subCategoriesReducer,
+  products: productsReducer,
 });
 export default rootReducer;
