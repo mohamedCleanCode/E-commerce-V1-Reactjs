0bcd04060354db5d17868da69cf5990a80279904
mohamed <m635379@gmail.com>
2023-03-21 14:52:29 +0200

    add route

--- src/components/Admin/AdminProductsManagmentCard.jsx
+++ src/components/Admin/AdminProductsManagmentCard.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const AdminProductsManagmentCard = ({ product }) => {
   return (
     <Col xs="10" sm="10" md="5" lg="3" className="mb-3 m-auto">
-      <Card className="position-relative" style={{ width: "100%" }}>
-        <div className="ddmin-products-managment-card-icons position-absolute">
-          <p>Edit</p>
-          <p>Remove</p>
-        </div>
-        <Card.Img
-          variant="top"
-          src={product?.imageCover}
-          style={{ width: "100%", height: "200px", objectFit: "contain" }}
-        />
-        <Card.Body>
-          <i className="fa-regular fa-heart"></i>
-          <Card.Title>{product?.title}</Card.Title>
-          <div className="info d-flex justify-content-between mt-3">
-            <p className="price m-0">${product?.price}</p>
-            <div className="rate" style={{ color: "gold" }}>
-              4.5 <i className="fa-solid fa-star"></i>
-            </div>
+      <Link
+        to={`/products/${product?._id}`}
+        style={{ color: "#444", textDecoration: "none" }}
+      >
+        <Card className="position-relative" style={{ width: "100%" }}>
+          <div className="ddmin-products-managment-card-icons position-absolute">
+            <p>Edit</p>
+            <p>Remove</p>
           </div>
-        </Card.Body>
-      </Card>
+          <Card.Img
+            variant="top"
+            src={product?.imageCover}
+            style={{ width: "100%", height: "200px", objectFit: "contain" }}
+          />
+          <Card.Body>
+            <i className="fa-regular fa-heart"></i>
+            <Card.Title>{product?.title}</Card.Title>
+            <div className="info d-flex justify-content-between mt-3">
+              <p className="price m-0">${product?.price}</p>
+              <div className="rate" style={{ color: "gold" }}>
+                4.5 <i className="fa-solid fa-star"></i>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </Link>
     </Col>
   );
 };
