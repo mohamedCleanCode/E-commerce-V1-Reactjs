a98b1de26088eb0dfba6b4463865f83cf1963bd1
mohamed <m635379@gmail.com>
2023-04-03 15:04:54 +0200

    add product ot wishlist

--- src/hook/wishlist/useProductCard.js
+++ src/hook/wishlist/useProductCard.js
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProductToWishList } from "../../redux/actions/wishlistActions";
 
 const useProductCard = (product) => {
+  const dispatch = useDispatch();
+  const wishlist = useSelector((state) => state.wishlist);
+
   const [color, setColor] = useState("#eee");
 
-  const handleFavorite = () => {
+  const handleFavorite = async () => {
     if (color === "#eee") {
       setColor("red");
+      await dispatch(
+        setProductToWishList({
+          productId: product?._id,
+        })
+      );
     }
     if (color === "red") {
       setColor("#eee");
+      console.log(color);
     }
   };
+
+  console.log(wishlist);
 
   return [color, handleFavorite];
 };
--- src/redux/actions/wishlistActions.js
+++ src/redux/actions/wishlistActions.js
@@ -1,9 +1,9 @@
 import { usePostData } from "../../hooks/usePostData";
 import { ERROR, SET_PRODUCT_TO_WISHLIST } from "../types";
 
-export const setProductToWishList = (productId) => async (dispatch) => {
+export const setProductToWishList = (formData) => async (dispatch) => {
   try {
-    const res = usePostData("/api/v1/wishlist", productId);
+    const res = await usePostData("/api/v1/wishlist", formData);
     dispatch({
       type: SET_PRODUCT_TO_WISHLIST,
       payload: res,
