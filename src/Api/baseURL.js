e176c69c41cba4294a00a38bbeba213bf0e819e1
mohamed <m635379@gmail.com>
2023-08-19 17:36:12 +0200

    fix bugs

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: "https://node-mongodb-api-1il0.onrender.com/",
+  baseURL: "https://node-mongodb-api-1il0.onrender.com",
 });
 
 export default baseURL;
